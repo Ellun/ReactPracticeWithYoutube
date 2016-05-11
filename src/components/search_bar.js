a6ddcd9ff0b3e09d25137f52a9fda623c34bf72e
Elton Cheng <eltoncheng86@gmail.com>
2016-05-11 18:02:50 -0400

    built youtube based application off tutorial.

---
+++ src/components/search_bar.js
@@ -0,0 +1,24 @@
+import React, {Component} from 'react';
+
+export default class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {term: ''};
+  }
+
+  render() {
+    return (
+      <div className="search-bar">
+        <input
+        value={this.state.term}
+        onChange={event => this.onInputchange(event.target.value)}/>
+      </div>
+    );
+  }
+
+  onInputchange(term) {
+    this.setState({term})
+    this.props.onSearchTermChange(term);
+  }
+}
